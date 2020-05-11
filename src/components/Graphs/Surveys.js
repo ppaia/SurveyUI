aed8d53b6fc6b52b89209a0f88dcdd0304e1ca24
arunmacys <arunkumar.manivasagam@cognizant.com>
2020-05-11 17:09:28 +0530

    Updated

--- src/components/Graphs/Surveys.js
+++ src/components/Graphs/Surveys.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Table from 'react-bootstrap/table';
+import { Table } from 'react-bootstrap';
 import Modal from "./Modal";
 
 class Surveys extends Component {
